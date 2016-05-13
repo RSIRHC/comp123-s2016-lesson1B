503175de13e917200fd7c65330aa0a1a2fa364a1
R.SIRHC <christopher.r99@live.com>
2016-05-13 10:35:41 -0400

    Add project files.

---
+++ comp123-s2016-lesson1B/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comp123_s2016_lesson1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
