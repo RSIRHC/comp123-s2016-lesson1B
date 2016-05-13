63b9915a8cc0078d22c61b0c3a8fdb3e14f18084
R.SIRHC <christopher.r99@live.com>
2016-05-13 10:43:50 -0400

    Initial Commit

--- comp123-s2016-lesson1B/Program.cs
+++ comp123-s2016-lesson1B/Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿//class#2: practice to perform how to do unit test and Github publish.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Author:Christopher Ritchil 
+ * Date: May 13, 2016
+ * Date Modified: 
+ * Student ID: 
+ * 
+ * Description: This program demonstrates Github and version control
+ * 
+ * Version: 0.1 - Initial version
+ */
 namespace comp123_s2016_lesson1B
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
